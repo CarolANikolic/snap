9442989dbf8fc67588edcf3b237c9b88c8161f52
Carol A. Nikolic <carolnikolic@gmail.com>
2022-12-28 15:02:40 -0300

    feature: control dropdown content for feature and company button

---
+++ script.js
@@ -0,0 +1,46 @@
+// Get buttons that contain dropdown
+
+const featuresBtn = document.getElementById('features');
+const companyBtn = document.getElementById('company');
+
+// Get dropdown content
+const featuresContent = document.getElementById('features-content');
+const companyContent = document.getElementById('company-content')
+
+
+// Control when dropdown content from Feature button will be displayed
+const collapseFeaturesContent = () => {
+    console.log('features');
+
+    if(featuresContent.style.display == 'block') {
+        featuresContent.style.display = 'none';
+        console.log(featuresContent.style.display);
+    } else  {
+        featuresContent.style.display = 'block';
+
+    } 
+}
+
+
+// Control when dropdown content from Company button will be displayed
+const collapseCompanyContent = () => {
+  
+    if(companyContent.style.display == 'block') {
+        companyContent.style.display = 'none';
+
+    } else  {
+        companyContent.style.display = 'block';
+    }  
+}
+
+
+// Event listener for Feature button
+featuresBtn.addEventListener('click', () => {
+    collapseFeaturesContent();  
+});
+
+// Event listener for Company button
+companyBtn.addEventListener('click', () => {
+    collapseCompanyContent();
+ });
+
